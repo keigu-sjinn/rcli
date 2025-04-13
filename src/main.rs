74e6747ccf42e7207ff2bc107aa6e16ba1fb607d
keigu-sjinn <keigu2008@outlook.com>
2025-04-13 13:56:47 +0800

    add red style to Error

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ use clap::{
 #[derive(Debug, Parser)]
 #[command(name="rcli",version, about, long_about = None,color = clap::ColorChoice::Always,
 styles = Styles::styled()
+.error(Style::new().fg_color(Some(AnsiColor::BrightRed.into())).effects(Effects::BOLD)) // 错误
 .header(anstyle::Style::new().fg_color(Some(AnsiColor::BrightGreen.into())).effects(Effects::BOLD)) // 标题
 .usage(Style::new().fg_color(Some(AnsiColor::BrightGreen.into())).effects(Effects::BOLD)) // 用法
 .literal(Style::new().fg_color(Some(AnsiColor::BrightCyan.into())).effects(Effects::BOLD)) // 字面量
